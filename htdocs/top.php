5ccf491d0efde2f4fb2844c80a447c843984051a
nisiyamanisi <s2242084ky@s.chibakoudai.jp>
2024-07-03 16:37:05 +0900

    a

---
+++ htdocs/top.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>サンプルページ</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="header">ここに文字が入ります</div>
+    <div class="links">
+        <a href="https://example1.com">リンク1</a>
+        <a href="https://example2.com">リンク2</a>
+    </div>
+</body>
+</html>
