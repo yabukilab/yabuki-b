55eac98dd05d1f62bc93ae676ff720e14a0c4c13
nisiyamanisi <s2242084ky@s.chibakoudai.jp>
2024-07-03 17:02:57 +0900

    a

---
+++ htdocs/black.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>背景真っ黒に赤文字</title>
+    <link rel="stylesheet" href="styles3.css">
+</head>
+<body>
+    <div class="centered-text">
+        <p>これは赤い文字です</p>
+    </div>
+    <div class="centered-button">
+        <button onclick="history.back()">戻る</button>
+    </div>
+</body>
+</html>
