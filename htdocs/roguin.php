613b82f45596f3bd5f163c98ec24a9f02e0076a4
nisiyamanisi <s2242084ky@s.chibakoudai.jp>
2024-07-03 16:54:13 +0900

    a

---
+++ htdocs/roguin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Page</title>
+    <link rel="stylesheet" href="styles2.css">
+</head>
+<body>
+    <div class="container">
+        <h1>中央の上部に文字</h1>
+        <div class="input-group">
+            <label for="input1">コメント1:</label>
+            <input type="text" id="input1" name="input1">
+        </div>
+        <div class="input-group">
+            <label for="input2">コメント2:</label>
+            <input type="text" id="input2" name="input2">
+        </div>
+    </div>
+    <div class="footer">
+        <a href="otherpage.php" class="button">他ページへのリンク</a>
+    </div>
+</body>
+</html>
