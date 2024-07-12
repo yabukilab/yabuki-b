7769ce070075a88aa2b8dba8caafc2602be2c736
yamanishiyama <s2242084@s.chibakoudai.jp>
2024-07-12 10:35:07 +0900

    a

--- mydb.sql
+++ mydb.sql
@@ -15,7 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 --
 -- Table structure for table `baseball_scores`
 --
@@ -38,7 +37,6 @@ CREATE TABLE `baseball_scores` (
 
 LOCK TABLES `baseball_scores` WRITE;
 /*!40000 ALTER TABLE `baseball_scores` DISABLE KEYS */;
-INSERT INTO `baseball_scores` VALUES (1,1,'0',0),(2,1,'0',1),(3,2,'1',0),(4,2,'1',1),(5,3,'0',0),(6,3,'0',1),(7,4,'2',0),(8,4,'0',1),(9,5,'0',0),(10,5,'0',1),(11,6,'0',0),(12,6,'5',1),(13,7,'1',0),(14,7,'0',1),(154,8,'0',0);
 /*!40000 ALTER TABLE `baseball_scores` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,32 +60,7 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (313,'1塁.jpg'),(314,'満塁.jpg'),(315,'1塁.jpg'),(316,'1塁.jpg'),(317,'ランナーなし.jpg'),(318,'満塁.jpg'),(319,'2.3塁.jpg'),(320,'1.3塁.jpg'),(321,'ランナーなし.jpg'),(322,'満塁.jpg'),(323,'2.3塁.jpg');
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `images_table`
---
-
-DROP TABLE IF EXISTS `images_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `images_table` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `inning` varchar(255) NOT NULL,
-  `image` longblob NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `images_table`
---
-
-LOCK TABLES `images_table` WRITE;
-/*!40000 ALTER TABLE `images_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `images_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -111,7 +84,6 @@ CREATE TABLE `red_circles` (
 
 LOCK TABLES `red_circles` WRITE;
 /*!40000 ALTER TABLE `red_circles` DISABLE KEYS */;
-INSERT INTO `red_circles` VALUES (14,20,20);
 /*!40000 ALTER TABLE `red_circles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,7 +109,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'5678','$2y$10$Kf7UuKqujzVmgPQdtpz/Euk4tkP3DBYfcQofKT3UTG45onEprBvTS'),(9,'0319','$2y$10$k.rLLeWnTGvWhEWlCxJf6.iYJq8UySw0JP321zJzO6tvhIQFKDvLi');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -150,4 +121,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-12  5:34:29
+-- Dump completed on 2024-07-12 10:34:58
