b02caa4fa6cf1c7e6027049216506a580cd14964
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-06-20 08:22:32 +0000

    test

--- htdocs/gamen5.php
+++ htdocs/gamen5.php
@@ -28,6 +28,16 @@ $title = $_GET['title'] ?? 'タイトル不明';
 
         <form action="submit.php" method="POST">
             <input type="hidden" name="title" value="<?= htmlspecialchars($title) ?>">
+
+
+            <label for="rating">評価</label>
+            <div class="star-rating">
+                 <input type="radio" id="star5" name="rating" value="5"><label for="star5">★</label>
+                 <input type="radio" id="star4" name="rating" value="4"><label for="star4">★</label>
+                 <input type="radio" id="star3" name="rating" value="3"><label for="star3">★</label>
+                 <input type="radio" id="star2" name="rating" value="2"><label for="star2">★</label>
+                 <input type="radio" id="star1" name="rating" value="1"><label for="star1">★</label>
+            </div>
             <label for="content">感想</label><br>
             <textarea id="content" name="content" rows="6" cols="50" placeholder="ここに感想を入力してください" required></textarea><br><br>
             <button type="submit" class="btn">投稿</button>
