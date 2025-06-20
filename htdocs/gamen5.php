c5e848693bca02363dd4eb790ae44bd7f296847e
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-06-20 05:51:18 +0000

    星評価

--- htdocs/gamen5.php
+++ htdocs/gamen5.php
@@ -16,6 +16,16 @@ $title = $_GET['title'] ?? 'タイトル不明';
         <h1>感想を投稿する</h1>
         <h2>作品タイトル：<?= htmlspecialchars($title) ?></h2>
 
+        <label for="rating">評価</label>
+        <div class="star-rating">
+                   <input type="radio" id="star5" name="rating" value="5"><label for="star5">★</label>
+                   <input type="radio" id="star4" name="rating" value="4"><label for="star4">★</label>
+                   <input type="radio" id="star3" name="rating" value="3"><label for="star3">★</label>
+                   <input type="radio" id="star2" name="rating" value="2"><label for="star2">★</label>
+                   <input type="radio" id="star1" name="rating" value="1"><label for="star1">★</label>
+        </div>
+
+
         <form action="submit.php" method="POST" class="form-wrapper">
             <input type="hidden" name="title" value="<?= htmlspecialchars($title) ?>">
             <label for="content">感想</label>
--- htdocs/submit.php
+++ htdocs/submit.php
@@ -3,7 +3,20 @@ session_start();
 
 $title = $_POST['title'] ?? 'タイトル不明';
 $content = $_POST['content'] ?? '';
+$rating = $_POST['rating'] ?? null;
 
 echo "<h2>投稿された感想</h2>";
 echo "<strong>作品タイトル：</strong> " . htmlspecialchars($title) . "<br><br>";
-echo "<strong>感想：</strong><br>" . nl2br(htmlspecialchars($content));
+echo "<strong>感想：</strong><br>" . nl2br(htmlspecialchars($content)) . "<br><br>";
+
+// 評価の表示
+if ($rating !== null) {
+    echo "<strong>評価：</strong> ";
+    for ($i = 1; $i <= 5; $i++) {
+        echo $i <= $rating ? "★" : "☆";
+    }
+    echo "（{$rating} / 5）";
+} else {
+    echo "<strong>評価：</strong> 評価なし";
+}
+?>
