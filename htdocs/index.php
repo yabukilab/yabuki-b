52809c9e6267b6ce30ad11a578fadd679b3c81d6
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-07 14:55:12 +0000

    ８

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,18 +3,18 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>読書記録交流アプリ</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container">
     <h1>読書記録交流<span class="accent">アプリ</span></h1>
-    <form>
+    <main>
       <button class="btn" onclick="location.href='touroku.php'">新規登録</button>
       <button class="btn" onclick="location.href='login.php'">ログイン</button>
-    </form>
+    </main>
 <body>
-</html>
 
 
 
