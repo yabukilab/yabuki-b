c634b99a7b75d3a9f20f0996baa4dbcb56b4add1
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-07 14:56:58 +0000

    ５５

--- htdocs/touroku.php
+++ htdocs/touroku.php
@@ -35,10 +35,9 @@ try {
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8">
   <title>新規登録</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container">
