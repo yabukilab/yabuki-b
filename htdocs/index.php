310f28b742d2da29e59e239b15a48b9c7e1a7c27
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-07 14:55:48 +0000

    ４４

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,7 +3,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
   <title>読書記録交流アプリ</title>
   <link rel="stylesheet" href="style.css">
 </head>
