7de05c5ad0368c1702088f6b8714876f5a20f32d
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-08 07:04:35 +0000

    mypagebtn

--- htdocs/kannsou.php
+++ htdocs/kannsou.php
@@ -31,6 +31,9 @@ if (isset($_GET['title'])) {
     <div class="container">
         <h1>感想を投稿する</h1>
         <h2>作品タイトル：<?= htmlspecialchars($title) ?></h2>
+          <div class="mypage-button">
+              <a href="mypage.php" class="btn">マイページ</a>
+           </div>
 
 
         <form action="submit.php" method="POST">
@@ -39,11 +42,11 @@ if (isset($_GET['title'])) {
 
             <label for="rating">評価</label>
             <div class="star-rating">
-                 <input type="radio" id="star5" name="rating" value="5"><label for="star5">★</label>
-                 <input type="radio" id="star4" name="rating" value="4"><label for="star4">★</label>
+                 <input type="radio" id="star5" name="rating" value="5"><label for="star1">★</label>
+                 <input type="radio" id="star4" name="rating" value="4"><label for="star2">★</label>
                  <input type="radio" id="star3" name="rating" value="3"><label for="star3">★</label>
-                 <input type="radio" id="star2" name="rating" value="2"><label for="star2">★</label>
-                 <input type="radio" id="star1" name="rating" value="1"><label for="star1">★</label>
+                 <input type="radio" id="star2" name="rating" value="2"><label for="star4">★</label>
+                 <input type="radio" id="star1" name="rating" value="1"><label for="star5">★</label>
             </div>
             <label for="content">感想</label><br>
             <textarea id="content" name="content" rows="6" cols="50" placeholder="ここに感想を入力してください" required></textarea><br><br>
@@ -57,9 +60,6 @@ if (isset($_GET['title'])) {
         </form>
     </div>
 
-    <!-- マイページボタン -->
-    <div class="mypage-button" style="text-align: right; margin-top: 20px;">
-        <a href="mypage.php" class="btn">マイページ</a>
-    </div>
+    
 </body>
 </html>
