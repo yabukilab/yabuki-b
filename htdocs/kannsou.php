16dc5c67403af54f5d47aa80049a74554eb7f17f
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-08 07:39:12 +0000

    t

--- htdocs/kannsou.php
+++ htdocs/kannsou.php
@@ -47,11 +47,11 @@ if (isset($_GET['title'])) {
 
             <label for="rating">評価</label>
             <div class="star-rating">
-                 <input type="radio" id="star1" name="rating" value="5"><label for="star1">★</label>
-                 <input type="radio" id="star2" name="rating" value="4"><label for="star2">★</label>
+                 <input type="radio" id="star1" name="rating" value="1"><label for="star1">★</label>
+                 <input type="radio" id="star2" name="rating" value="2"><label for="star2">★</label>
                  <input type="radio" id="star3" name="rating" value="3"><label for="star3">★</label>
-                 <input type="radio" id="star4" name="rating" value="2"><label for="star4">★</label>
-                 <input type="radio" id="star5" name="rating" value="1"><label for="star5">★</label>
+                 <input type="radio" id="star4" name="rating" value="4"><label for="star4">★</label>
+                 <input type="radio" id="star5" name="rating" value="5"><label for="star5">★</label>
             </div>
             <label for="content">感想</label><br>
             <textarea id="content" name="content" rows="6" cols="50" placeholder="ここに感想を入力してください" required></textarea><br><br>
