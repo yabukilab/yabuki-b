906b75c5bbae12ab69e17e87b6affd13ba3def7f
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-09 05:51:34 +0000

    db接続

--- htdocs/tasyanokannsou.php
+++ htdocs/tasyanokannsou.php
@@ -6,7 +6,22 @@ session_start();
 $title = $_GET['title'] ?? 'タイトル不明';
 $reviews = [];
 
-require_once 'db.php';
+$dbServer = '127.0.0.1';
+$dbUser = isset($_SERVER['MYSQL_USER'])     ? $_SERVER['MYSQL_USER']     : 'testuser';
+$dbPass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'pass';
+$dbName = isset($_SERVER['MYSQL_DB'])       ? $_SERVER['MYSQL_DB']       : 'mydb';
+
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+} catch (PDOException $e) {
+    exit('データベース接続エラー: ' . htmlspecialchars($e->getMessage()));
+}
 
 
 try {
