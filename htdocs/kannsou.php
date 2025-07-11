901340bdef501ad28712e8b99a127b9c12b90ae3
honda masahiro <s2242104KP@s.chibakoudai.jp>
2025-07-11 03:44:13 +0000

    星評価修正

--- htdocs/kannsou.php
+++ htdocs/kannsou.php
@@ -37,11 +37,11 @@ $keyword = $_GET['q'] ?? '';
 
             <label for="rating">評価</label>
             <div class="star-rating">
-                <input type="radio" id="star1" name="rating" value="1" /><label for="star1">★</label>
-                <input type="radio" id="star2" name="rating" value="2" /><label for="star2">★</label>
-                <input type="radio" id="star3" name="rating" value="3" /><label for="star3">★</label>
-                <input type="radio" id="star4" name="rating" value="4" /><label for="star4">★</label>
                 <input type="radio" id="star5" name="rating" value="5" /><label for="star5">★</label>
+                <input type="radio" id="star4" name="rating" value="4" /><label for="star4">★</label>
+                <input type="radio" id="star3" name="rating" value="3" /><label for="star3">★</label>
+                <input type="radio" id="star2" name="rating" value="2" /><label for="star2">★</label>
+                <input type="radio" id="star1" name="rating" value="1" /><label for="star1">★</label>
             </div>
 
             <label for="content">感想</label><br>
